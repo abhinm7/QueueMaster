cba7fea5ae098cd388f12e8db9e1cbb4fd2ac76d
ABHIN M <abhin18m@gmail.com>
2026-03-23 15:20:10 +0530

    expose authentication routes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    
+    constructor(private authService: AuthService) { }
+
+    @Post('register')
+    async register(@Body() body: any) {
+        return this.authService.register(body.email, body.password);
+    }
+
+    @Post('login')
+    async login(@Body() body: any) {
+        const user = await this.authService.validateUser(body.email, body.password);
+        return this.authService.login(user);
+    }
 }
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -5,7 +5,7 @@ import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule,UsersService],
   providers: [UsersService],
 })
 export class UsersModule {}
