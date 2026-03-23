c0ecd070be5c81a7e7f77d0dcd994b02008248a0
ABHIN M <abhin18m@gmail.com>
2026-03-23 15:13:35 +0530

    add auth services and jwt configs

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { jwtStrategy } from './jwt.strategy';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (ConfigService: ConfigService) => ({
+        secret: ConfigService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' }
+      })
+    })
+  ],
+  providers: [AuthService, jwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-   constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService 
-  ) {}
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) { }
 
+    async register(email: string, pass: string) {
+        const existing = await this.usersService.findByEmail(email);
+        if (existing) throw new ConflictException('Email already in use');
+
+        const hashedPassword = await bcrypt.hash(pass, 10);
+        const user = await this.usersService.create({ email, password: hashedPassword });
+
+        return this.login(user);
+    }
+
+    async login(user: any) {
+        const payload = { email: user.email, sub: user.id };
+        return {
+            accessToken: this.jwtService.sign(payload)
+        }
+    }
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.usersService.findByEmail(email);
+        if (user && await bcrypt.compare(pass, user.password)) {
+            const { password, ...result } = user;
+            return result;
+        }
+        throw new UnauthorizedException('Invalid credentials')
+    }
 }
